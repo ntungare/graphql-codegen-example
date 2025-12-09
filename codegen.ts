c58b6822fba16f1b1761b177550b6a9e9051aff3
Ninad Tungare <ninad.tungare@gmail.com>
2025-12-09 18:03:43 +0530

    refresh

--- codegen.ts
+++ codegen.ts
@@ -1,23 +1,21 @@
-import type { CodegenConfig } from "@graphql-codegen/cli";
+import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-    require: ["ts-node/register"],
+    require: ['ts-node/register'],
     generates: {
-        "src/generated/": {
-            schema: "https://countries.trevorblades.com/",
-            documents: "src/query/**/*.ts",
-            plugins: [],
-            preset: "gql-tag-operations-preset",
+        'src/generated/': {
+            schema: 'https://countries.trevorblades.com/',
+            documents: 'src/query/**/*.ts',
+            preset: 'client-preset',
+            plugins: [
+                {
+                    add: {
+                        content: '/* oxlint-disable */',
+                    },
+                },
+            ],
             presetConfig: {
-                augmentedModuleName: "graphql-tag",
-            },
-            hooks: {
-                afterOneFileWrite: ["npx prettier --write"],
-            },
-            config: {
-                dedupeFragments: true,
-                dedupeOperationSuffix: true,
-                exportFragmentSpreadSubTypes: true,
+                augmentedModuleName: 'graphql-tag',
             },
         },
     },
